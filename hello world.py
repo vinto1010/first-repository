069d4eecf394eda9e698791c1079e0b56702d6ec
vinto1010 <jayaja1010@gmail.com>
2025-09-12 13:16:33 +0900

    Third commit

--- hello world.py
+++ hello world.py
@@ -1,9 +1,10 @@
-def print_hello():
-    print("Hello World")
+def add(x,y):
+    su = x+y
+    return su
 
 def print_KIT():
-    print("Kumoh National Institute of Technology.")
+    print("KIT.")
 
 if __name__ == "__main__":
     print_KIT()
-    print_hello()
+    print( add(1,2) )
